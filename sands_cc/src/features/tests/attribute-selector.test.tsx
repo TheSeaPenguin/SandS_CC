dc093ec0308bf0f00aaa137b46f28a235a24a5f3
TheSeaPenguin <seapenguin248@gmail.com>
2025-01-06 18:39:41 +0000

    Update tests for attributes and attribute selector

--- sands_cc/src/features/tests/attribute-selector.test.tsx
+++ sands_cc/src/features/tests/attribute-selector.test.tsx
@@ -1,6 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import { AttributeSelector } from '../stats';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+
+// TODO: Fix accompanying errors with state updates
 describe('Attribute-selector', () => {
   const mockProps = {
     vertical: true,
@@ -20,11 +23,65 @@ describe('Attribute-selector', () => {
     expect(screen.getByText(/Fire/i)).toBeVisible();
     expect(screen.getByText(/Air/i)).toBeVisible();
 
-    expect(screen.getAllByRole('button').length).toEqual(12);
+    expect(screen.getAllByRole('button').length).toEqual(8);
+    expect(screen.getAllByRole('combobox').length).toEqual(4);
+
     expect(screen.getAllByText(/Modifier/i).length).toEqual(4);
     expect(screen.getAllByText(/Total/i).length).toEqual(4);
 
     expect(screen.getAllByText('+ 0').length).toEqual(4);
     expect(screen.getAllByText('0').length).toEqual(4);
+  });
+
+  it('Should only show values attribute values not already selected', async () => {
+    const User = userEvent.setup();
+    const comboBoxes = screen.getAllByRole('combobox');
+
+    for (let i = 0; i < comboBoxes.length; i++) {
+      await User.click(comboBoxes[i]);
+      expect(screen.getByRole('listbox')).toBeVisible();
+      const listValues = screen.getAllByRole('option');
+      expect(listValues.length).toEqual(5 - i);
+      await User.click(listValues[1]);
+    }
+  });
+
+  it('Should update improvement point modifiers', async () => {
+    const User = userEvent.setup();
+    let improvementPointsText = screen.getByTestId(
+      'text-improvementPointsValue'
+    ).textContent;
+    const plusButton = screen.getAllByRole('button', { name: '+' })[0];
+    const minusButton = screen.getAllByRole('button', { name: '-' })[0];
+
+    expect(improvementPointsText).toEqual('0/3');
+
+    for (let i = 1; i < 4; i++) {
+      await User.click(plusButton);
+      improvementPointsText = screen.getByTestId(
+        'text-improvementPointsValue'
+      ).textContent;
+      expect(improvementPointsText).toEqual(i + '/3');
+    }
+
+    await User.click(plusButton);
+    improvementPointsText = screen.getByTestId(
+      'text-improvementPointsValue'
+    ).textContent;
+    expect(improvementPointsText).not.toEqual('4/3');
+
+    for (let i = 2; i > -1; i--) {
+      await User.click(minusButton);
+      improvementPointsText = screen.getByTestId(
+        'text-improvementPointsValue'
+      ).textContent;
+      expect(improvementPointsText).toEqual(i + '/3');
+    }
+
+    await User.click(minusButton);
+    improvementPointsText = screen.getByTestId(
+      'text-improvementPointsValue'
+    ).textContent;
+    expect(improvementPointsText).not.toEqual('-1/3');
   });
 });
--- sands_cc/src/features/tests/attributes.test.tsx
+++ sands_cc/src/features/tests/attributes.test.tsx
@@ -1,16 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, renderHook, screen } from '@testing-library/react';
 import { Attributes } from '../stats';
 import '@testing-library/jest-dom';
+import { useStore } from '../../hooks/useStore';
 
 describe('Attributes', () => {
   const mockProps = {
     vertical: false,
     spacing: 2
   };
+
+  const initialStoreState = useStore.getState();
+
   beforeEach(() => {
+    useStore.setState(initialStoreState, true);
     render(<Attributes {...mockProps} />);
   });
-
   it('Should render', () => {
     expect(screen.getByText(/Earth/i)).toBeVisible();
     expect(screen.getByText(/Water/i)).toBeVisible();
@@ -18,5 +22,29 @@ describe('Attributes', () => {
     expect(screen.getByText(/Air/i)).toBeVisible();
 
     expect(screen.getAllByText('0').length).toEqual(4);
+  });
+
+  it('Should display correct store values', async () => {
+    const getInitialState = () => {
+      const { result } = renderHook(() =>
+        useStore((state) => state.attributes.earth)
+      );
+      return result.current.toString();
+    };
+    let earthBox = screen.getByTestId('EarthStatBoxValue');
+    expect(earthBox.textContent).toEqual(getInitialState());
+
+    renderHook(() => useStore((state) => state.updateAttribute('earth', 25)));
+
+    const getSecondState = () => {
+      const { result } = renderHook(() =>
+        useStore((state) => state.attributes.earth)
+      );
+      return result.current.toString();
+    };
+
+    earthBox = screen.getByTestId('EarthStatBoxValue');
+    expect(earthBox.textContent).toEqual(getSecondState());
+    expect(earthBox.textContent).toEqual('25');
   });
 });
